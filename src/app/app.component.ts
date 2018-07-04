d5091fc78886635113af0ef4577b0042508aeb8c
Boghdan <bogdkovalov@gmail.com>
2018-07-04 18:21:28 +0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'rss-reader-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'rss-reader';
+}
